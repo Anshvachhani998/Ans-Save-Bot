81e747d00231f100ff1b0e7c4255152c6d591457
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-10-08 14:36:45 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,44 @@
+
+
+import os
+
+SESSION = "savebot"
+API_ID = int(os.getenv("API_ID", "8012239"))
+API_HASH = os.getenv("API_HASH", "171e6f1bf66ed8dcc5140fbe827b6b08")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8391245987:AAG1p_xqznPSfUaBjggqrWEQcoQoCJlRn6U")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002926368982"))
+DUMP_CHANNEL_ID = int(os.getenv("DUMP_CHANNEL_ID", "-1002890289206"))
+JIO_DUMP = int(os.getenv("JIO_DUMP", "-1003022720180"))
+PORT = int(os.getenv("PORT", "8080"))
+FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "-1002849184248"))
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Ansh089:Ansh089@cluster0.y8tpouc.mongodb.net/?retryWrites=true&w=majority")
+MONGO_NAME = os.getenv("MONGO_NAME", "SpotifyDL")
+
+MONGO_URI_2 = os.getenv("MONGO_URI_2", "mongodb+srv://ftmbotzx:ftmbotzx@cluster0.0b8imks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_NAME_2 = os.getenv("MONGO_NAME_2", "SpotifyDL")
+
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "medias")
+ADMINS = [5660839376, 6167872503, 5961011848, 6538627123, 8104777494]
+OWNER_ID = 7744665378
+DAILY_LIMITS = 20
+MAINTENANCE_MODE = False
+USER_SESSION = "BQB6Qc8APy91zoBtQGv-O19FCUpBvrwkiwXohHi5nolHHv04HduDe6oKPaDRNvIiBfCcURe2SQYTO4oj-qWP-Cqpd5Bbq70xnIxiZsLS4-Al4fQxhl3mI59CUKxwIg0Iure6-BkkyXm7A-oLeatLz5UR7RcGBt6QdIdElHOS6cxR8DoJIwqtFfWqb9szJjsYLD-r66wMF8IrGA4aC0lcXybyf8OS5wRGNqnuq-LcvheIcy_HVSIJtDIJLsLyOsAjGlJLItKPDr_m88keBSrTDMgZEOIKqPSJoPfiqjOwYrrhC_imty5T7SUmAiFbIcP8KOxDJj0yW7e2DVUrxyg-kG3GboCDOgAAAAFY13jAAA"
+USERBOT_CHAT_ID = 5785483456
+FAILD_CHAT_ID = int(os.getenv("FAILD_CHAT_ID", "-1002849184248"))
+GROUP_ID = [-1002526173846, -1002508944571, -1002713034219]
+REQ_CHANNEL = -1003186496490
+
+MAX_PLAYLIST = 500
+USE_SPOTIFY_API = True
+DELETE_CHANNELS = -1003036780293
+MAINTENANCE_MESSAGE = (
+    "⚠️ **Maintenance Mode Activated** ⚙️\n\n"
+    "Our bot is currently undergoing scheduled maintenance to improve performance and add new features.\n\n"
+    "Please check back in a while. We’ll be back soon, better than ever!\n\n"
+    "💬 **Support Group:** [SUPPORT](https://t.me/AnSBotsSupports)\n\n"
+    "**– Team Support**"
+)
+
+
+FORCE_SUB_MODE = False  # ✅ Enable/Disable force join
+FORCE_JOIN = "AnsMusicHQ"
