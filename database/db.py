6df25ec6696ceabace7f2003b24837e3cdc349ce
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-10-14 13:51:30 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -8,7 +8,7 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.users
+        self.col = self.db["newusers"]
 
 
     def new_user(self, id, name):
